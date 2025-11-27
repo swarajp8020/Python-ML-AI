40bdecdf30a33c3bda82907241e74b8eafd5c35e
SwarajXO <purarkarswaraj@gmail.com>
2025-11-27 06:04:44 +0530

    if-else

--- program1.py
+++
@@ -1,9 +0,0 @@
-# Write a program that asks for your name and age, calculates the year you turn 100, and prints it using an f-string.
-name = input("Enter Name: ")
-age_string = input("How Old are you? ")
-age = int(age_string)
-current_year = 2025
-years_left = 100-age
-turn_100_year = current_year + years_left
-print(f"Hi{name}{age}")
-print(f"You will turn 100 years old in the year {turn_100_year}.")
