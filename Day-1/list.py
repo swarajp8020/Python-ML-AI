7d14af111098442938045bfa39d5ac7a13dd9159
SwarajXO <purarkarswaraj@gmail.com>
2025-11-27 07:30:17 +0530

    added in folder

---
+++ Day-1/list.py
@@ -0,0 +1,7 @@
+heros = ["spiderman","thor","ironman"]
+heros.append("swaraj")
+print(heros[-1])
+avengers_team = heros[0:5]
+print(f"main team {avengers_team}")
+heros.remove("thor")
+print(f"final team {heros}")
