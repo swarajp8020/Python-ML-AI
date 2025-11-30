f58c12a1d9cdf7686f00994a612f6595c45fa9b6
SwarajXO <purarkarswaraj@gmail.com>
2025-11-30 11:04:13 +0530

    30-11-25 practice

--- Day-3/practice.py
+++ Day-3/practice.py
@@ -7,13 +7,13 @@ weather_data = {
         {"day": "Thu", "temp": 27, "rain": True}
     ]
 }
-daily_list = weather_data["forecast"]
+daily_list=weather_data["forecast"]
 total_temp = 0
 rainy_days = 0
 for day_list in daily_list:
-    total_temp+=day_list["temp"]
-    if day_list["rain"] == True:
-        rainy_days += 1
-avg_time = total_temp/len(daily_list)
-print(f"average time {avg_time}")
+    total_temp += day_list["temp"]
+    if day_list["rain"]==True:
+        rainy_days+=1
+avg_days = total_temp/len(daily_list)
+print(f"avg days {avg_days}")
 print(f"rainy days {rainy_days}")
