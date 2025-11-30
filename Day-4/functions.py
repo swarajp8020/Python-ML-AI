d91556da4d911054820a2d77ddd9e12d5a586171
SwarajXO <purarkarswaraj@gmail.com>
2025-11-30 11:25:56 +0530

    Default Arguments

--- Day-4/functions.py
+++ Day-4/functions.py
@@ -1,5 +1,23 @@
-def greet_user(name):
-    message = f"Hello, {name}! Welcome to AI Engineering"
-    return message
-print(greet_user("Swaraj"))
-print(greet_user("Google"))
+# Defined using 'def'
+# def greet_user(name):
+#     # Logic is indented
+#     message = f"Hello, {name}! Welcome to AI Engineering."
+#     return message
+
+# # Usage (Outside the indentation)
+# print(greet_user("Swaraj"))
+# print(greet_user("Google"))
+
+# 'tax_rate' defaults to 0.10 (10%) if the user doesn't provide it
+def calculate_salary(base, tax_rate=0.10):
+    tax = base * tax_rate
+    net_salary = base - tax
+    return net_salary
+
+# Scenario 1: Use default tax (10%)
+s1 = calculate_salary(100000) 
+print(f"Standard Salary: {s1}")
+
+# Scenario 2: Custom tax (30%)
+s2 = calculate_salary(100000, 0.30)
+print(f"Taxed Salary: {s2}")
