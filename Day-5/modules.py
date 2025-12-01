c079da753e690461fc3b146e921d375790e280a9
SwarajXO <purarkarswaraj@gmail.com>
2025-12-01 15:08:57 +0530

    1-dec-2025

---
+++ Day-5/modules.py
@@ -0,0 +1,10 @@
+import random, math, datetime
+# 1. Random: Pick a random winner
+team = ["swaraj","sahil","ravindra","sangita"]
+winner = random.choice(team)
+print(f"The Winner is {winner}")
+# 2. Datetime: What time is it?
+now = datetime.datetime.now()
+print(f"Current Log Time: {now}")
+# 3. Math: Square Root
+print(f"Square root of 16 is: {math.sqrt(16)}")
