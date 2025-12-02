02eeff6cf75baa1ed85e4d8a9e9120ee349c2159
SwarajXO <purarkarswaraj@gmail.com>
2025-12-02 13:23:38 +0530

    practice

--- practice.py
+++ practice.py
@@ -2,7 +2,7 @@ import random
 def roll_dice(sides):
     try:
         sides = int(sides)
-        if sides > 2:
+        if sides <2:
             return "you need atleast 2 sides"
         return random.randint(1,sides)
     except ValueError:
