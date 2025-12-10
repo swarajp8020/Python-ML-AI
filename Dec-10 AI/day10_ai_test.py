fa2b153a7ab5b4c0792b23b40d801954d3fb3fdb
SwarajXO <purarkarswaraj@gmail.com>
2025-12-10 19:23:54 +0530

    practice

--- Dec-10 AI/day10_ai_test.py
+++ Dec-10 AI/day10_ai_test.py
@@ -6,11 +6,11 @@ genai.configure(api_key=GEMINI_API_KEY)
 print("--- Contacting Gemini... ---")
 
 # ATTEMPT 1: The generic 'flash' alias (Usually the safest bet)
-model = genai.GenerativeModel('gemini-flash-latest') 
+model = genai.GenerativeModel('gemini-flash-latest')
 
 # IF THAT FAILS, try un-commenting this one:
 # model = genai.GenerativeModel('models/gemini-2.0-flash-exp')
 
 response = model.generate_content("Explain what Python is in 1 sentence.")
 
-print(f"\nAI Says:\n{response.text}")
+print(f"\nSwaraj Says:\n{response.text}")
