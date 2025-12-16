2c3a85c3585de23a59c9820e822048315ed6fe5f
SwarajXO <purarkarswaraj@gmail.com>
2025-12-16 05:59:07 +0530

    automated

--- AI_Project/client_test.py
+++ AI_Project/client_test.py
@@ -1,49 +1,28 @@
-# import requests # The tool to visit websites via code
-
-# url = "http://127.0.0.1:8000/review"
-
-# filename = input("Enter file to fix: ")
-# try:
-#     with open(filename,"r")as f:
-#         content_of_file = f.read()
-#         my_data = { "code": content_of_file }
-
-#     print("--- Sending Order to Kitchen ---")
-
-#     response = requests.post(url, json=my_data)
-
-#     result = response.json()
-#     # Save the result to a new file
-#     output_name = "fixed_" + filename
-#     with open(output_name, "w") as f:
-#         f.write(result["fixed_code"])
-        
-#     print(f"--- Saved to {output_name} ---")
-#     print("--- Chef's Answer ---")
-#     print(result["fixed_code"])
-# except FileNotFoundError:
-#     print("Error: file not found")
-
-import requests 
+import requests # The tool to visit websites via code
+import os
 
 url = "http://127.0.0.1:8000/review"
+files = os.listdir(".")
+print(f"Scanning Folders: {os.getcwd()}")
+for file in files:
+    if file.startswith("buggy_")and file.endswith(".py"):
+        print(f"\n--- Found Buggy File: {file} ---")
+        try:
+                with open(file,"r")as f:
+                    content_of_file = f.read()
+                    my_data = { "code": content_of_file }
 
-filename = input("enter a file to fix: ")
-try:
-    with open(filename, "r") as f:
-        content_of_file = f.read()
-        my_data = {"code" : content_of_file}
-    response = requests.post(url, json=my_data)
-    result = response.json()
-    
-    print("--saved--")
+                    print("--- Sending Order to Kitchen ---")
 
-    output_name = "fixed_"+filename
-    with open(output_name, "w") as f:
-        f.write(result["fixed_code"])
-    print(f"done {output_name}")
-    print("answer")
-    print(result["fixed_code"])
+                    response = requests.post(url, json=my_data)
 
-except FileNotFoundError:
-    print("Error! Please add correct file. ")
+                    result = response.json()
+                        # Save the result to a new file
+                    output_name = "fixed_" + file
+                    with open(output_name, "w") as f:
+                        f.write(result["fixed_code"])
+                            
+                    print(f"--- Saved to {output_name} ---")
+        except Exception as e:
+            print(f"Error: file not found {file}: {e}")
+
