60b5ba55e1cccf6452638eb93cd1a737c0c340dc
SwarajXO <purarkarswaraj@gmail.com>
2025-12-19 06:38:30 +0530

    19-12-2025 practice client refactored code

---
+++ AI_Project/practice_code.py
@@ -0,0 +1,38 @@
+import os, sys, requests
+
+def scan_and_fix(folder_path):
+    print(f"--Scanning {folder_path}")
+    url = "http://127.0.0.1:8000/review"
+    files = os.listdir(folder_path)
+    print(f"scanning folders {os.getcwd()}")
+    for file in files:
+        if file.startswith("buggy_") and file.endswith(".py"):
+            print(f"\n Found Buggy File: {file}")
+            full_file_path = os.path.join(folder_path, file)
+            try:
+                with open(full_file_path, "r") as f:
+                    content_of_file = f.read()
+                    my_data = {"code":content_of_file}
+                    print("--sending order to kitchen--")
+                    response = requests.post(url, json=my_data)
+                if response.status_code == 200:
+                    result = response.json()
+                    output_name = "fixed_"+file
+                    output_path = os.path.join(folder_path, output_name)
+                    
+                    with open(output_path, "w")as f:
+                        f.write(result["fixed_code"])
+                    print(f"saved--{output_name}")
+                else:
+                    print(f"Server error:{response.status_code}")
+            except Exception as e:
+                print(f"Error: file not found! {file}:{e}")
+def main():
+    if len(sys.argv)>1:
+            folder_to_scan = sys.argv[1]
+    else:
+        folder_to_scan = "."
+    scan_and_fix(folder_to_scan)
+# The "On" Button
+if __name__ == "__main__":
+    main()
